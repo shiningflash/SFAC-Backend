1be16ec0f8d03d87c895223163ac795c14ba3b54
Amirul Islam <amirulislamalmamun@gmail.com>
2021-04-08 10:13:52 +0600

    Delete secret_info.py

--- secret_info.py
+++
@@ -1,8 +0,0 @@
-SECRET_KEY = '8c@$huw#(e@xd2fz#dbavg^h(*3is9t$$7gm9t&h45i%&xfy#z'
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.mailtrap.io'
-EMAIL_PORT = 2525
-# EMAIL_USE_TLS = True
-EMAIL_HOST_USER = '02700ded0bcbcc'
-EMAIL_HOST_PASSWORD = 'ce3bb0f7ec6a4e'
